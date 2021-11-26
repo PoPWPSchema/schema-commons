de5eb10b0b5d4b89b2cbdbc20755734f03093a4a
leoloso <leo@getpop.org>
2021-11-26 07:33:59 +0000

    Merge pull request #1251 from leoloso/Move-DateQuery-from-CustomPosts-to-SchemaCommons
    Move DateQuery from CustomPosts to SchemaCommons

---
+++ src/Overrides/TypeResolvers/InputObjectType/DateQueryInputObjectTypeResolver.php
@@ -0,0 +1,160 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PoPWPSchema\SchemaCommons\Overrides\TypeResolvers\InputObjectType;
+
+use PoP\Engine\TypeResolvers\ScalarType\BooleanScalarTypeResolver;
+use PoP\Engine\TypeResolvers\ScalarType\IntScalarTypeResolver;
+use PoP\Engine\TypeResolvers\ScalarType\StringScalarTypeResolver;
+use PoPSchema\SchemaCommons\TypeResolvers\InputObjectType\DateQueryInputObjectTypeResolver as UpstreamDateQueryInputObjectTypeResolver;
+use PoPWPSchema\SchemaCommons\TypeResolvers\EnumType\RelationEnumTypeResolver;
+use stdClass;
+
+class DateQueryInputObjectTypeResolver extends UpstreamDateQueryInputObjectTypeResolver
+{
+    private ?BooleanScalarTypeResolver $booleanScalarTypeResolver = null;
+    private ?IntScalarTypeResolver $intScalarTypeResolver = null;
+    private ?StringScalarTypeResolver $stringScalarTypeResolver = null;
+    private ?RelationEnumTypeResolver $relationEnumTypeResolver = null;
+
+    final public function setBooleanScalarTypeResolver(BooleanScalarTypeResolver $booleanScalarTypeResolver): void
+    {
+        $this->booleanScalarTypeResolver = $booleanScalarTypeResolver;
+    }
+    final protected function getBooleanScalarTypeResolver(): BooleanScalarTypeResolver
+    {
+        return $this->booleanScalarTypeResolver ??= $this->instanceManager->getInstance(BooleanScalarTypeResolver::class);
+    }
+    final public function setIntScalarTypeResolver(IntScalarTypeResolver $intScalarTypeResolver): void
+    {
+        $this->intScalarTypeResolver = $intScalarTypeResolver;
+    }
+    final protected function getIntScalarTypeResolver(): IntScalarTypeResolver
+    {
+        return $this->intScalarTypeResolver ??= $this->instanceManager->getInstance(IntScalarTypeResolver::class);
+    }
+    final public function setStringScalarTypeResolver(StringScalarTypeResolver $stringScalarTypeResolver): void
+    {
+        $this->stringScalarTypeResolver = $stringScalarTypeResolver;
+    }
+    final protected function getStringScalarTypeResolver(): StringScalarTypeResolver
+    {
+        return $this->stringScalarTypeResolver ??= $this->instanceManager->getInstance(StringScalarTypeResolver::class);
+    }
+    final public function setRelationEnumTypeResolver(RelationEnumTypeResolver $relationEnumTypeResolver): void
+    {
+        $this->relationEnumTypeResolver = $relationEnumTypeResolver;
+    }
+    final protected function getRelationEnumTypeResolver(): RelationEnumTypeResolver
+    {
+        return $this->relationEnumTypeResolver ??= $this->instanceManager->getInstance(RelationEnumTypeResolver::class);
+    }
+
+    public function getInputFieldNameTypeResolvers(): array
+    {
+        return array_merge(
+            parent::getInputFieldNameTypeResolvers(),
+            [
+                'inclusive' => $this->getBooleanScalarTypeResolver(),
+                'year' => $this->getIntScalarTypeResolver(),
+                'month' => $this->getIntScalarTypeResolver(),
+                'week' => $this->getIntScalarTypeResolver(),
+                'day' => $this->getIntScalarTypeResolver(),
+                'hour' => $this->getIntScalarTypeResolver(),
+                'minute' => $this->getIntScalarTypeResolver(),
+                'second' => $this->getIntScalarTypeResolver(),
+                'compare' => $this->getStringScalarTypeResolver(),
+                'column' => $this->getStringScalarTypeResolver(),
+                'relation' => $this->getRelationEnumTypeResolver(),
+            ]
+        );
+    }
+
+    public function getInputFieldDescription(string $inputFieldName): ?string
+    {
+        return match ($inputFieldName) {
+            'inclusive' => $this->getTranslationAPI()->__('For after/before, whether exact value should be matched or not', 'schema-commons'),
+            'year' => $this->getTranslationAPI()->__('4 digit year (e.g. 2011)', 'schema-commons'),
+            'month' => $this->getTranslationAPI()->__('Month number (from 1 to 12)', 'schema-commons'),
+            'week' => $this->getTranslationAPI()->__('Week of the year (from 0 to 53)', 'schema-commons'),
+            'day' => $this->getTranslationAPI()->__('Day of the month (from 1 to 31)', 'schema-commons'),
+            'hour' => $this->getTranslationAPI()->__('Hour (from 0 to 23)', 'schema-commons'),
+            'minute' => $this->getTranslationAPI()->__('Minute (from 0 to 59)', 'schema-commons'),
+            'second' => $this->getTranslationAPI()->__('Second (0 to 59)', 'schema-commons'),
+            'compare' => $this->getTranslationAPI()->__('Determines and validates what comparison operator to use', 'schema-commons'),
+            'column' => $this->getTranslationAPI()->__('Posts column to query against. Default: ‘post_date’)', 'schema-commons'),
+            'relation' => $this->getTranslationAPI()->__('OR or AND, how the sub-arrays should be compared. Default: AND. Only the value from the first sub-array will be used', 'schema-commons'),
+            default => parent::getInputFieldDescription($inputFieldName),
+        };
+    }
+
+    /**
+     * Integrate parameters into the "date_query" WP_Query arg
+     *
+     * @see https://developer.wordpress.org/reference/classes/wp_query/#date-parameters
+     *
+     * @param array<string, mixed> $query
+     * @param stdClass|stdClass[]|array<stdClass[]> $inputValue
+     */
+    public function integrateInputValueToFilteringQueryArgs(array &$query, stdClass|array $inputValue): void
+    {
+        /**
+         * Collect all the "date_query" results, and then arrange them properly
+         * as an array, with the "relation" as the first element (if defined)
+         */
+        if (is_array($inputValue)) {
+            $innerQueries = [];
+            parent::integrateInputValueToFilteringQueryArgs($innerQueries, $inputValue);
+            $query['date_query'] = [];
+            // The "relation" is defined on the first element
+            if (isset($innerQueries[0]['date_query']['relation'])) {
+                $query['date_query']['relation'] = $innerQueries[0]['date_query']['relation'];
+            }
+            // Re-create an array with all the subelements
+            foreach ($innerQueries as $innerQuery) {
+                $query['date_query'][] = $innerQuery['date_query'];
+            }
+            return;
+        }
+
+        /**
+         * Here it's a single stdClass. Create the config for a single "date_query"
+         */
+        $dateQuery = [];
+
+        // These elements must be serialized, from Date to String
+        if (isset($inputValue->before)) {
+            $dateQuery['before'] = $this->getDateScalarTypeResolver()->serialize($inputValue->before);
+        }
+        if (isset($inputValue->after)) {
+            $dateQuery['after'] = $this->getDateScalarTypeResolver()->serialize($inputValue->after);
+        }
+
+        // These elements can copy directly
+        $properties = [
+            'year',
+            'month',
+            'week',
+            'day',
+            'hour',
+            'minute',
+            'second',
+            'inclusive',
+            'compare',
+            'column',
+            'relation',
+        ];
+        foreach ($properties as $property) {
+            if (!isset($inputValue->$property)) {
+                continue;
+            }
+            $dateQuery[$property] = $inputValue->$property;
+        }
+
+        // Assign under "date_query"
+        if ($dateQuery !== []) {
+            $query['date_query'] = $dateQuery;
+        }
+    }
+}
---
+++ src/SchemaHooks/AbstractConvertDateQueryInputFieldToArrayInputObjectTypeHookSet.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PoPWPSchema\SchemaCommons\SchemaHooks;
+
+use PoP\ComponentModel\Schema\SchemaTypeModifiers;
+use PoP\ComponentModel\TypeResolvers\InputObjectType\HookNames;
+use PoP\ComponentModel\TypeResolvers\InputObjectType\InputObjectTypeResolverInterface;
+use PoP\Hooks\AbstractHookSet;
+
+abstract class AbstractConvertDateQueryInputFieldToArrayInputObjectTypeHookSet extends AbstractHookSet
+{
+    protected function init(): void
+    {
+        $this->getHooksAPI()->addFilter(
+            HookNames::INPUT_FIELD_TYPE_MODIFIERS,
+            [$this, 'getInputFieldTypeModifiers'],
+            10,
+            3
+        );
+    }
+
+    abstract protected function isInputObjectTypeResolver(InputObjectTypeResolverInterface $inputObjectTypeResolver): bool;
+
+    /**
+     * Transform "dateQuery" from a single value to an array of them
+     */
+    public function getInputFieldTypeModifiers(
+        int $inputFieldTypeModifiers,
+        InputObjectTypeResolverInterface $inputObjectTypeResolver,
+        string $inputFieldName
+    ): int {
+        if (!$this->isInputObjectTypeResolver($inputObjectTypeResolver)) {
+            return $inputFieldTypeModifiers;
+        }
+        return match ($inputFieldName) {
+            'dateQuery' => SchemaTypeModifiers::IS_ARRAY | SchemaTypeModifiers::IS_NON_NULLABLE_ITEMS_IN_ARRAY,
+            default => $inputFieldTypeModifiers,
+        };
+    }
+}
